d798b548262cb4561132fa8642ef9dfeaa9b8f1d
ltmyth <latimmark@gmail.com>
2018-09-27 15:59:35 +0300

    class work 1

---
+++ class1.php
@@ -0,0 +1,45 @@
+<?php
+
+	class Pazon{
+		private $Firstname = 'Neyo';
+		private $Surname = 'Mad';
+
+		function getFirstname(){
+			return($this->Firstname);
+		}
+
+		function getSurname(){
+			return($this->Surname);
+		}
+	}
+
+?>
+
+<?php
+	class Employee{
+		public $name = 'dizel';
+		public $startdate;
+		public $gender;
+	}
+
+	$emp = new Employee();
+	$emp->name = 'John';
+	$emp->startdate = "25-04-2018";
+	$emp->gender = 'male';
+
+	echo("<h1>".$emp->name."</h1>"." ");
+	echo("is"." ".$emp->gender);
+
+
+	/*class Receptionist extends Employee{
+		function bookslot(){
+
+		}
+	}
+
+	class Doctor extends Employee{
+		function prescribedrug(){
+
+		}
+	}*/
+?>
